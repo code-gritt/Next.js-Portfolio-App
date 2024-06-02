6eea10b494a338f9c152de152b45d6fe42175d3b
Gokul <gokulchandan24@gmail.com>
2024-06-02 20:34:17 +0530

    added bento grid

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
 import { FloatingNav } from "@/components/FloatingNavbar";
 import Footer from "@/components/Footer";
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
@@ -12,6 +13,7 @@ const Home = () => {
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
+        <Grid />
         <RecentProjects />
         <Clients />
         <Experience />
